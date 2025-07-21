7d0253215b286ca6e8ca4e2def23254ce0919b98
MrThomasLibrary <beethoven@MyLaptop.somethingstupid.org>
2025-07-20 23:06:49 -0700

    callnumber is working

---
+++ text-version/CallNumber.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+class CallNumber {
+  private:
+  std::string gClass;
+  float subject;
+  std::string cutter;
+  int copy;
+  std::string fullCallNumber;
+
+  public:
+  CallNumber(const std::string gc, const float s, const std::string c, const int cy);
+  ~CallNumber();
+  int compare(const CallNumber& cn) const;
+  std::string getFullCallNumber() const;
+};
